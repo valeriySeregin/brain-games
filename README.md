47b1c338f75a06a812141a0d06aecc4ad80d7004
Valeriy Seregin <valeriy@seregin.io>
2018-09-10 21:22:11 +0300

    added brain-even game, changed README.md with asciinema

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Brain Games
+# Brain Games hexlet project
 <a href="https://codeclimate.com/github/valeriySeregin/project-lvl1-s344/maintainability"><img src="https://api.codeclimate.com/v1/badges/02d5875d6169d702253a/maintainability" /></a>
 <a href="https://codeclimate.com/github/valeriySeregin/project-lvl1-s344/test_coverage"><img src="https://api.codeclimate.com/v1/badges/02d5875d6169d702253a/test_coverage" /></a>
 <a href="https://travis-ci.org/valeriySeregin/project-lvl1-s344"><img src="https://travis-ci.org/valeriySeregin/project-lvl1-s344.svg?branch=master"></a>
+
+## installing
+<a href="https://asciinema.org/a/rzgGWQWwRy6XKWylrNOUNcBSu" target="_blank"><img src="https://asciinema.org/a/rzgGWQWwRy6XKWylrNOUNcBSu.png" /></a>
+
+## brain-even gaming
+<a href="https://asciinema.org/a/T42abX5tFKcXE97QLRBXTqful" target="_blank"><img src="https://asciinema.org/a/T42abX5tFKcXE97QLRBXTqful.png" /></a>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import readlineSync from 'readline-sync';
 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const isEven = num => num % 2 === 0;
+const gameRounds = 3;
+
 export default () => {
   console.log('Welcome to the Brain Games!');
-  console.log();
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  console.log(`Hello, ${name}!`);
+
+  const game = (counter) => {
+    if (counter === gameRounds) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+    let newCounter = counter;
+    const question = getRandomNumber(1, 100);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
+
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      newCounter += 1;
+      game(newCounter);
+      return;
+    }
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+  };
+
+  return game(0);
 };
